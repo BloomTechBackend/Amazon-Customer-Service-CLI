8f3a5cc97877f6e99232749587f84403ebd6a00b
marh-ali <aam261@Georgetown.edu>
2022-02-22 10:37:10 -0500

    began MT4. Will finish Sprint 1 and return

--- src/com/amazon/ata/deliveringonourpromise/orderfulfillmentservice/OrderFulfillmentServiceClient.java
+++ src/com/amazon/ata/deliveringonourpromise/orderfulfillmentservice/OrderFulfillmentServiceClient.java
@@ -1,4 +1,18 @@
 package com.amazon.ata.deliveringonourpromise.orderfulfillmentservice;
 
+import com.amazon.ata.deliveringonourpromise.dao.PromiseDao;
+import com.amazon.ata.deliveringonourpromise.types.Promise;
+import com.amazon.ata.orderfulfillmentservice.OrderFulfillmentService;
+
+/**
+ * Client for accessing OrderFulfillment Service
+ */
+
 public class OrderFulfillmentServiceClient {
+    /* constructor that prevents object instantiation*/
+        private OrderFulfillmentServiceClient(){}
+
+    public static Promise getPromiseFromOrderFulfillmentService (String ) {
+            return new
+    }
 }
