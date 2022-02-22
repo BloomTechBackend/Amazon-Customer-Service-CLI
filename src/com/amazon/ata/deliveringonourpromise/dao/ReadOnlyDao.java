bbb06969f828a9366c273d8feb21b916f50c6dfd
marh-ali <aam261@Georgetown.edu>
2022-02-22 08:51:23 -0500

    no changes

--- src/com/amazon/ata/deliveringonourpromise/dao/ReadOnlyDao.java
+++ src/com/amazon/ata/deliveringonourpromise/dao/ReadOnlyDao.java
@@ -8,8 +8,6 @@ public interface ReadOnlyDao<I, O> {
     /**
      * Get object method to be implemented.
      * @param orderId Order Id
-     * @param I a param
-     * @param O a param
      * @return Object abstracted object
      */
     O get(I orderId);
